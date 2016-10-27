2e20197cde33d26123eff1290754c7113e23469a
Andrei Vacaruc <avacaruc@YOPESOs-iMac.local>
2016-10-27 15:20:15 +0300

    Fix tests#1
    Seems working correct, but doesn't suit for Taylor

--- Source/SourceKittenFramework/Request.swift
+++ Source/SourceKittenFramework/Request.swift
@@ -297,7 +297,7 @@ public enum Request {
         var values = Array(dict.values)
         return sourcekitd_request_dictionary_create(&keys, &values, dict.count)
     }
-
+    
     /**
     Create a Request.CursorInfo.sourcekitObject() from a file path and compiler arguments.
 
@@ -312,7 +312,7 @@ public enum Request {
         }
         return nil
     }
-
+    
     /**
     Send a Request.CursorInfo by updating its offset. Returns SourceKit response if successful.
 
@@ -328,7 +328,7 @@ public enum Request {
         sourcekitd_request_dictionary_set_int64(cursorInfoRequest, sourcekitd_uid_get_from_cstr(SwiftDocKey.offset.rawValue), offset)
         return try? Request.customRequest(request: cursorInfoRequest).failableSend()
     }
-
+    
     /**
     Sends the request to SourceKit and return the response as an [String: SourceKitRepresentable].
 
@@ -395,6 +395,9 @@ public enum Request {
         }
         return fromSourceKit(sourcekitd_response_get_value(response!)) as! [String: SourceKitRepresentable]
     }
+        
+    
+    
 }
 
 // MARK: CustomStringConvertible
@@ -523,3 +526,4 @@ extension Request.Error {
     @available(*, unavailable, renamed: "unknown(_:)")
     public static func Unknown(_: String?) -> Request.Error { fatalError() }
 }
+
